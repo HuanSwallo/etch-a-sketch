55d50534bd74edc921efd61db5804032a7057e93
HuanSwallo <tannernguyen561@gmail.com>
2025-07-02 22:40:47 -0700

    added a click and erase mode. added css elements making it neater. edited the README to guide on how to use it.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # etch-a-sketch
 A project where I used my DOM manipulation skills to mimic an etch-a-sketch toy using html, css, and javascript.
+# How to Use
+Hover over the grid to sketch. In the modes section, there are 2 modes called rainbow and shade mode. Rainbow mode generates random colors to the grid. Shade generates shades of black grids that darkens until it turns black. If you want to click on your grids, you can use click mode where it uses the selected custom color and generates the grids by either clicking or clicking while dragging. If you want to erase your grid, you can use erase mode where it erases a selected grid. Erase can also work with click mode where you can either click on a specific grid to erase or hover the specific grid to erase. \
+In the design section, you can reset the grid to the desired size from 1-100. You can change the selected color if you were to go into click mode.
+# What I have learned
+1. Create a grid using flexbox in javascript
+2. Learn how to use addEventListeners for different mouse events
+3. Learn how to change color and opacity from a classList
+4. Learn how to create edge cases from switching to different modes
--- script.js
+++ script.js
@@ -1,26 +1,93 @@
+// Consants
 const reset = document.getElementById("reset");
 const rainbow = document.getElementById("rainbow");
 const shade = document.getElementById("shade");
+const click = document.getElementById("click");
+const eraser = document.getElementById("eraser")
+const colorPicker = document.getElementById("color-picker");
 const defaultSize = 16;
-
+// Global Variables
 let currentMode = "rainbow";
+let prevMode = "rainbow";
+let drawing = false;
+let clickOn = false;
+let eraserOn = false;
+let customColor = "#000000";
+let useCustomColor = false;
+
+// Global Event Listeners for the mouse state
+document.body.addEventListener('mousedown', () => drawing = true);
+document.body.addEventListener('mouseup', () => drawing = false);
 
 rainbow.addEventListener('click', () => {
-    shade.style.background = "#b8b9bc";
-    shade.style.color = "#000000";
     currentMode = "rainbow";
-    rainbow.style.background = "#00bbff";
-    rainbow.style.color = "azure";
+    useCustomColor = false;
+    shade.classList.remove("toggled");
+    eraser.classList.remove("toggled");
+    rainbow.classList.add("toggled");
 });
 
 shade.addEventListener('click', () => {
-    rainbow.style.background = "#b8b9bc";
-    rainbow.style.color = "#000000";
     currentMode = "shade";
-    shade.style.background = "#00bbff";
-    shade.style.color = "azure";
+    useCustomColor = false;
+    rainbow.classList.remove("toggled");
+    eraser.classList.remove("toggled");
+    shade.classList.add("toggled");
 });
 
+click.addEventListener('click', () => {
+    clickOn = !clickOn;
+
+    if (clickOn) {
+        // Saves previous mode
+        if (rainbow.classList.contains("toggled")) {
+            prevMode = rainbow;
+        } else if (shade.classList.contains("toggled")) {
+            prevMode = shade;
+        }
+
+        useCustomColor = true;
+        click.classList.add("toggled");
+    } else {
+        click.classList.remove("toggled");
+        // Toggle previous mode whenever eraser and click is off
+        if (!eraserOn && prevMode) {
+            prevMode.classList.add("toggled");
+            currentMode = prevMode.id;
+        }
+    }
+});
+
+eraser.addEventListener('click', () => {
+    eraserOn = !eraserOn;
+
+    if (eraserOn) {
+        // Saves previous mode
+        if (rainbow.classList.contains("toggled")) {
+            prevMode = rainbow;
+        } else if (shade.classList.contains("toggled")) {
+            prevMode = shade;
+        }
+
+        rainbow.classList.remove("toggled");
+        shade.classList.remove("toggled");
+        eraser.classList.add("toggled");
+    } else {
+        eraser.classList.remove("toggled");
+        // Toggle previous mode whenever eraser and click is off
+        if (!clickOn && prevMode) {
+            prevMode.classList.add("toggled");
+            currentMode = prevMode.id;
+        }
+    }
+    
+});
+
+colorPicker.addEventListener('input', (e) => {
+    customColor = e.target.value;
+});
+
+// Resets and creates a new grid
 reset.addEventListener('click', () => {
     let newSize = prompt("Enter new grid size (1–100):");
     newSize = parseInt(newSize);
@@ -41,33 +108,54 @@ function getRandomRGB() {
     return `rgb(${r}, ${g}, ${b})`;
 }
 
+// Draws the square based on the conditions
+function doDraw(square) {
+    if (eraserOn) {
+        square.style.backgroundColor = "";
+        square.style.opacity = "";
+    } else if (clickOn) {
+        square.style.backgroundColor = customColor;
+        square.style.opacity = "1";
+    } else if (currentMode == "rainbow") {
+        square.style.background = getRandomRGB();
+    } else if (currentMode == "shade") {
+        let shadeCount = parseFloat(square.style.opacity) || 0; // If opacity not set, set it to 0
+        shadeCount = Math.min(shadeCount + 0.1, 1);
+        square.style.backgroundColor = "#000000";
+        square.style.opacity = shadeCount;
+    }
+}
+
 function makeGrid(size) {
     const screen = document.querySelector(".screen");
-    screen.innerHTML = "";
+    screen.innerHTML = ""; // Clears the grid
 
-    const squareSize = 500 / size;
+    const squareSize = 500 / size; // Compute the size of the square
 
     for (let i = 0; i < size * size; i++) {
         const square = document.createElement("div");
         square.classList.add("square");
         square.style.width = `${squareSize}px`;
         square.style.height = `${squareSize}px`;
-
-        square.dataset.hoverCount = 0;
+        
+        //Event when left mouse button is pressed down
+        square.addEventListener("mousedown", (e) => {
+            if (e.buttons !== 1) return; // Do nothing if mouse buttons other than left mouse triggers
+            doDraw(square);
+        });
+        //Event when mouse hovers over element. Also works with mousedown (mousedown + mouseover)
         square.addEventListener('mouseover', (e) => {
-            let count = parseInt(square.dataset.hoverCount);
-            if (currentMode == "rainbow") {
-                square.style.background = getRandomRGB();
-            }
-            else if (currentMode == "shade") {
-                let shadeCount = parseFloat(square.style.opacity) || 0; // If opacity not set, set it to 0
-                shadeCount = Math.min(shadeCount + 0.1, 1);
-                square.style.backgroundColor = "#000000";
-                square.style.opacity = shadeCount;
-            }
+            if (clickOn && !drawing) return; // Do nothing if click mode is on and drawing is false (mouse is not down)
+            doDraw(square);
+        });
+        // Event when a square is clicked
+        square.addEventListener("click", () => {
+            doDraw(square);
         });
         screen.appendChild(square);
     }
 }
-
-makeGrid(defaultSize);
+//Creates grid by default
+window.onload = () => {
+    makeGrid(defaultSize);
+};
